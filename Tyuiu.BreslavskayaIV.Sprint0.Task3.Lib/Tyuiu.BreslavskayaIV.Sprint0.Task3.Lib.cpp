7fbf7fae373a0f14b1583e1793a85742c795c160
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-08 19:53:26 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp
@@ -0,0 +1,15 @@
+// Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service :public ISprint0Task2V0
+{
+	virtual int Add(int a, int b) override {
+		return a + b;
+
+	}
+};
---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task3.Test/Tyuiu.BreslavskayaIV.Sprint0.Task3.Test.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.BreslavskayaIV.Sprint0.Task2.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task2.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task2V0* service = new Service();
+			int a = 3;
+			int b = 4;
+			int c;
+			c = service->Add(a, b);
+			Assert::AreEqual(7, c);
+		}
+	};
+}
+
