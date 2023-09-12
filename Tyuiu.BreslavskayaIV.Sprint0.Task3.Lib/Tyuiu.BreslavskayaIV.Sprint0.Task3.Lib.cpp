40e4a9152b3b763c89085ae013cc02e5e427449f
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-12 11:01:25 +0300

    Add files via upload

--- Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp
+++ Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp
@@ -1,15 +1,15 @@
-// Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp : Defines the functions for the static library.
+﻿// Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp : Defines the functions for the static library.
 //
 
 #include "pch.h"
 #include "framework.h"
-#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
 
 // TODO: This is an example of a library function
-class Service :public ISprint0Task2V0
+class Service1 : public ISprint0Task3V0
 {
-	virtual int Add(int a, int b) override {
-		return a + b;
-
-	}
+	virtual int Add(int a, int b, int c) override
+	{
+		return a + b + c;
+	};
 };
