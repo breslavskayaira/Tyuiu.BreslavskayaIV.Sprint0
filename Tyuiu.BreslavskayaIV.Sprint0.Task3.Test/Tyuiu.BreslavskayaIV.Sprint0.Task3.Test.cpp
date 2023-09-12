b8356356d454ef70a4913e0b54f5c99ce528075e
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-12 11:01:04 +0300

    Add files via upload

--- Tyuiu.BreslavskayaIV.Sprint0.Task3.Test/Tyuiu.BreslavskayaIV.Sprint0.Task3.Test.cpp
+++ Tyuiu.BreslavskayaIV.Sprint0.Task3.Test/Tyuiu.BreslavskayaIV.Sprint0.Task3.Test.cpp
@@ -1,24 +1,26 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "CppUnitTest.h"
-#include "../Tyuiu.BreslavskayaIV.Sprint0.Task2.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task2.Lib.cpp"
+#include "../Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task3.Lib.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTest1
+namespace UnitTest2
 {
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(UnitTest2)
 	{
 	public:
-
+		
 		TEST_METHOD(TestMethod1)
 		{
-			ISprint0Task2V0* service = new Service();
+			ISprint0Task3V0* date = new Service1();
 			int a = 3;
 			int b = 4;
-			int c;
-			c = service->Add(a, b);
-			Assert::AreEqual(7, c);
+			int c = 5;
+			int d;
+
+			d = date->Add(a, b, c);
+
+			Assert::AreEqual(12, d);
 		}
 	};
 }
-
