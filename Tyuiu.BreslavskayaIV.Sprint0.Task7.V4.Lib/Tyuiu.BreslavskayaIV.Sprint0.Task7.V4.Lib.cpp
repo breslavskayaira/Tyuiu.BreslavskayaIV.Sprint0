a979ecc4cea4e03367e86f2be09b2584c5f432d5
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-14 18:29:53 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib.cpp
@@ -0,0 +1,25 @@
+﻿// Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+class ISprint0Task7
+{
+public: 
+	virtual float Rezalt(int k, int a)
+{
+	int k1 =  k / 1000000;
+	int k2 = (k / 100000)%10;
+	int k3 = (k / 10000) % 10;
+	int k4 = (k / 1000) % 10;
+	int k5 = (k / 100) % 10;
+	int k6 = (k / 10) % 10;
+	bool p = (k1 == a) || (k2 == a) || (k3 == a) || (k4 == a) || (k5 == a) || (k6 == a);
+	return p;
+}
+
+
+};
+
---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Test/Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVSprint0Task7V4Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVSprint0Task7V4Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
