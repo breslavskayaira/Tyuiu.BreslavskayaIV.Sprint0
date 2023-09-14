4445f2a11962230d97063e7d6f845684c1b40dbc
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-14 15:44:16 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+class Isprint0Task4
+{
+public:
+	virtual int Culc(int a, int b, int c, int d) {
+		return a * b + c * d;
+	}
+		
+};
---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test/Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVSprint0Task4V4Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVSprint0Task4V4Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
