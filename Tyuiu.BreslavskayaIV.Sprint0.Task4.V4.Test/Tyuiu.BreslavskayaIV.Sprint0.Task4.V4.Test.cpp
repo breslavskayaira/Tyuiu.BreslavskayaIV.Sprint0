35d747e2dfc2ef9faa10be7b61ff4eb201b525c5
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-14 16:08:29 +0300

    Update Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test.cpp

--- Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test/Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test.cpp
+++ Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test/Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test.cpp
@@ -11,6 +11,14 @@ namespace TyuiuBreslavskayaIVSprint0Task4V4Test
 		
 		TEST_METHOD(TestMethod1)
 		{
+			ISprint0Task4* date = new ISprint0Task4();
+			int a = 2;
+			int b = 3;
+			int c = 4;
+			int d = 7;
+			int f;
+			f = date-> Culc(a, b, c, d);
+			Assert::AreEqual(7, c);
 		}
 	};
 }
