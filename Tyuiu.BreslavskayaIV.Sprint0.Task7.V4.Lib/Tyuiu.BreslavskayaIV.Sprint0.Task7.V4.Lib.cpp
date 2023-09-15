da7ff79bcedc3f9d5bf4932de5f4e9b3d1aee200
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-15 18:46:17 +0300

    Update Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib.cpp

--- Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib.cpp
+++ Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib.cpp
@@ -10,13 +10,13 @@ class ISprint0Task7
 public: 
 	virtual float Rezalt(int k, int a)
 {
-	int k1 =  k / 1000000;
-	int k2 = (k / 100000)%10;
-	int k3 = (k / 10000) % 10;
-	int k4 = (k / 1000) % 10;
-	int k5 = (k / 100) % 10;
-	int k6 = (k / 10) % 10;
-	bool p = (k1 == a) || (k2 == a) || (k3 == a) || (k4 == a) || (k5 == a) || (k6 == a);
+	int n1 =  k / 1000000;
+	int n2 = (k / 100000)%10;
+	int n3 = (k / 10000) % 10;
+	int n4 = (k / 1000) % 10;
+	int n5 = (k / 100) % 10;
+	int n6 = (k / 10) % 10;
+	bool p = (n1 == a) || (n2 == a) || (n3 == a) || (n4 == a) || (n5 == a) || (n6 == a);
 	return p;
 }
 
